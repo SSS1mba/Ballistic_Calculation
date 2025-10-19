d8e119385e4ae3bc9df07d3d7705ca0ad3b19e63
gms <makno5ov@yandex.ru>
2025-10-19 21:26:03 +0300

    Собрал все функции в один txt файл

---
+++ Ballistic_Calculation/Benchmark.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <ctime>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+using Clock = high_resolution_clock;
+
+template <typename Dur>
+void prettyPrintDuration(Dur dur)
+{
+    auto h = duration_cast<hours>(dur);
+    if (h.count()) { std::cout << h << " "; dur -= h; }
+
+    auto m = duration_cast<minutes>(dur);
+    if (m.count()) { std::cout << m << " "; dur -= m; }
+
+    auto s = duration_cast<seconds>(dur);
+    if (s.count()) { std::cout << s << " "; dur -= s; }
+
+    auto ms = duration_cast<milliseconds>(dur);
+    if (ms.count()) { std::cout << ms << " "; dur -= ms; }
+
+    auto us = duration_cast<microseconds>(dur);
+    if (us.count()) { std::cout << us << " "; dur -= us; }
+
+    auto ns = duration_cast<nanoseconds>(dur);
+    if (ns.count()) { std::cout << ns << " "; dur -= ns; }
+
+    std::cout << std::endl;
+}
+
+template<typename Func, typename... Args>
+auto benchmark(Func&& f, Args... args) -> decltype(f(std::forward<Args>(args)...))
+{
+    auto start = Clock::now();
+
+    auto summarize = [&start]() {
+        auto dt = Clock::now() - start;
+        prettyPrintDuration(dt);
+        };
+
+    if constexpr (std::is_same_v<decltype(f(std::forward<Args>(args)...)), void>) {
+        f(std::forward<Args>(args)...);
+        summarize();
+    }
+    else {
+        auto res = f(std::forward<Args>(args)...);
+        summarize();
+        return res;
+    }
+}
